92955f6b9205d236fe88f0d4aa23f5fdb26ee3ee
simon-benigeri <simon.benigeri@protonmail.ch>
2021-03-02 00:37:39 -0600

    updated weights and added some code to main to run experiments.

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from models.models import *
 from pipelines.run_dqn import train, evaluate
-from torch import tensor
 import matplotlib.pyplot as plt
-import math
-import os
 import yaml
 
-def run_experiment():
-    # creates model
-    #
-    pass
+with open("config.yml", "r") as ymlfile:
+    config = yaml.load(ymlfile)
 
 def load_weights(model:DQN, IN_PATH):
-    model.policy_net.load_state_dict(torch.load(PATH))
+    model.policy_net.load_state_dict(torch.load(IN_PATH))
     model.transfer_weights()
     return model
 
@@ -20,18 +15,29 @@ def save_weights(model:DQN, OUT_PATH):
     torch.save(model.target_net.state_dict(), OUT_PATH)
     return
 
-if __name__ == '__main__':
-    # Get all config values and hyperparameters
-    with open("config.yml", "r") as ymlfile:
-        config = yaml.load(ymlfile)
+def run_evaluations(model:DQN, dataset:str, eval_set:str):
+    profits, running_profits, total_profits = evaluate(model,
+                                                       dataset=dataset,
+                                                       evaluation_set=eval_set,
+                                                       strategy=config["STRATEGY"],
+                                                       only_use_strategy=False)
+    hold_profits, hold_running_profits, hold_total_profits = evaluate(model,
+                                                                      dataset=dataset,
+                                                                      evaluation_set=eval_set,
+                                                                      strategy=config["STRATEGY"],
+                                                                      only_use_strategy=True)
 
-    PATH = 'weights/numq_gspc_100.pt'
-    model = DQN(method=NUMQ)
+    print(f"TOTAL MKT PROFITS : {hold_total_profits}")
+    print(f"TOTAL MODEL PROFITS : {total_profits}")
+    plt.plot(list(range(len(running_profits))), running_profits, label="Model strategy")
+    plt.plot(list(range(len(hold_running_profits))), hold_running_profits, label="Buy and hold")
+    plt.legend()
+    plt.title("Profits")
+    plt.show()
+    return
 
-    model.policy_net.load_state_dict(torch.load(PATH))
-    model.transfer_weights()
-
-    model, losses, rewards, profits = train(model, dataset='gspc')
+def run_training(model:DQN, dataset:str):
+    model, losses, rewards, profits = train(model, dataset=dataset)
 
     plt.plot(list(range(len(losses))), losses)
     plt.title("Losses")
@@ -45,21 +51,48 @@ if __name__ == '__main__':
     plt.title("Total Profits")
     plt.show()
 
-    PATH = 'weights/not_numq_gspc_20.pt'
-    torch.save(model.target_net.state_dict(), PATH)
+    return
 
-    """
-    model = DQN(NUMQ)
-    model.policy_net.load_state_dict(torch.load(PATH))
-    model.transfer_weights()
-    """
+def run_experiment(**kwargs):
+    model = DQN(method=experiment_args['method'])
 
-    profits, running_profits, total_profits = evaluate(model, dataset='gspc', evaluation_set='test', strategy=config["STRATEGY"], only_use_strategy=False)
-    hold_profits, hold_running_profits, hold_total_profits = evaluate(model, dataset='gspc', evaluation_set='test', strategy=config["STRATEGY"], only_use_strategy=True)
+    if kwargs['load_model'] and kwargs['IN_PATH']:
+        load_weights(model=model, IN_PATH=kwargs['IN_PATH'])
 
-    print(f"TOTAL PROFITS : {total_profits}")
-    plt.plot(list(range(len(running_profits))), running_profits, label="Model strategy")
-    plt.plot(list(range(len(hold_running_profits))), hold_running_profits, label="Buy and hold")
-    plt.legend()
-    plt.title("Profits")
-    plt.show()
+    if kwargs['train_model'] and kwargs['dataset']:
+        model, losses, rewards, profits = train(model, dataset=kwargs['dataset'])
+        plt.plot(list(range(len(losses))), losses)
+        plt.title("Losses")
+        plt.show()
+
+        plt.plot(list(range(len(rewards))), rewards)
+        plt.title("Rewards")
+        plt.show()
+
+        plt.plot(list(range(len(profits))), profits)
+        plt.title("Total Profits")
+        plt.show()
+
+        if kwargs['save_model'] and kwargs['OUT_PATH']:
+            save_weights(model=model, OUT_PATH=kwargs['OUT_PATH'])
+
+    if kwargs['eval_model']:
+        run_evaluations(model=model, dataset=kwargs['dataset'], eval_set=kwargs['eval_set'])
+
+    return
+
+if __name__ == '__main__':
+    # Input your experiment params
+    experiment_args = {
+        'method': NUMQ,
+        'dataset': 'gspc',
+        'train_model': False,
+        'eval_model': True,
+        'eval_set': 'test',
+        'load_model': True,
+        'IN_PATH': 'weights/numq_gspc_60.pt',
+        'save_model': False,
+        'OUT_PATH': 'weights/numq_gspc_90.pt'
+    }
+
+    run_experiment(**experiment_args)
