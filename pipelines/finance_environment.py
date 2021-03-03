9bfee5b7e59b2d3d84dd3a8a8c358b72f9356989
simon-benigeri <simon.benigeri@protonmail.ch>
2021-03-03 03:05:50 -0600

    updated get state

--- pipelines/finance_environment.py
+++ pipelines/finance_environment.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
 import yaml
+import os
 
 from .build_batches import load_prices
-
-
 
 class FinanceEnvironment:
     def __init__(self, prices):
@@ -34,28 +33,43 @@ def _read_csv(index: str, ticker: str) -> pd.DataFrame:
     file_path = os.path.join(config["DATA_DIRECTORY"], file_name)
     return pd.read_csv(file_path)
 
-def _get_state(prices: pd.DataFrame, step: int, n: int, price_col:str, end_date: str):
+def _get_state(prices: pd.DataFrame, step: int, n: int, price_col:str, end_date: str, start_date: str=''):
     """
     :param prices: price data for this stock
     :param step: Timestep t
     :param n: LOOKBACK
     :param price_col: column corresponding to closing trade price in the df (differs for eurostoxx) #TODO: Homogenize data
     :param end_date: date at which we end the episode
+    :param start_date: date at which we end the episode
     :return:
     """
+    # TODO: DISCUSS FACT THAT THIS REQUIRES KEEPING FULL CSV IN MEMORY
+    #  Maybe we can just return restricted dataframe from read_csv
+    #  eg. df = read_csv => smaller_df = keep_dates(df)
+    #  also if we do this we dont need the dates to determine the end of an episode
+
     # initialize end
     end = False
+    # we get the start index based on start_date.
+    # this is useful if you are testing, then set start date to that date.
+    start_index = prices['Date'].get_loc[start_date] if start_date else 0
 
-    #TODO: PAD THIS DATAFRAME. IF LOOKBACK > STEP, WE ARE FUCKED
+    # if lookback > n we are in trouble,
+    # so we pad this dataframe with rows identical to the first row
+    padding = prices.at[0]*n
+    prices = pd.concat([padding, prices], axis=1, ignore_index=True)
 
-    # look up price in df at index step
+    # update step value because of padding and start index
+    step = step + n + start_index
+
+    # look up price, prev price, init price in df at indices step, step-1, step-n
     p_t = prices[price_col].at[step]
-    # look up prev price in df at index step-1
     p_t_prev = prices[price_col].at[step-1]
-    # look up init price in df at index step-n
     p_init = prices[price_col].at[step-n]
-    # we take the difference in prices along the column for indexes step-n to step
+
+    # take the difference in prices along the column for indexes step-n to step
     s_t = prices[price_col].diff(1).loc[step-n:step]
+
     # check the date at index step.
     # If it's the end date, we are at the end of the episode
     if prices['Date'].at[step] == end_date:
