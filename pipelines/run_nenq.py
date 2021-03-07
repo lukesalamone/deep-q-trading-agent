70530264c12b39c776ceab5a19e9ad0602a6e60e
simon-benigeri <simon.benigeri@protonmail.ch>
2021-03-07 14:35:00 -0600

    run renq progress. still need plots to be saved, and final training

--- pipelines/run_nenq.py
+++ pipelines/run_nenq.py
@@ -3,6 +3,8 @@ from pipelines.run_numq import train, evaluate
 from models.models import *
 from typing import List, Dict
 import yaml
+import json
+import os
 
 with open("config.yml", "r") as ymlfile:
     config = yaml.load(ymlfile, Loader=yaml.FullLoader)
@@ -16,12 +18,24 @@ def save_weights(model: DQN, OUT_PATH):
     torch.save(model.target_net.state_dict(), OUT_PATH)
     return
 
-def pretrain_on_group(profits: Dict, groups: Dict, index:str, method: str, group:str):
+def pretrain_on_group(groups: Dict, index:str, method: str, group:str):
+    group_total_profits = []
+    experiment_log = {
+        'name': f"{index}.{method}.{group}",
+        'index': index,
+        'method': method,
+        'group': group,
+        'symbol': {},
+        'total profits': 0
+    }
+
     for i, symbol in enumerate(groups[index][method][group]):
         model = DQN(NUMQ)
         if i > 0:
             previous_weights = f'weights/numq/{index}/{method}/{group}/numq_{symbol}_{i}.pt'
             model = load_weights(model=model, IN_PATH=previous_weights)
+
+        print(f"Start Train on group: {group}, index: {index}, method: {method}, symbol: {symbol} ... ")
         model, losses, rewards, val_rewards, profits, val_profits = train(model=model,
                                                                           index=index,
                                                                           symbol=symbol,
@@ -30,23 +44,66 @@ def pretrain_on_group(profits: Dict, groups: Dict, index:str, method: str, group
                                                                           path=config['STONK_PATH'],
                                                                           splits=config['STONK_INDEX_SPLITS'])
 
-        rewards, profits, running_profits, total_profit = evaluate(model=model,
-                                                                   index=index,
-                                                                   symbol=symbol,
-                                                                   dataset='valid',
-                                                                   path=config['STONK_PATH'],
-                                                                   splits=config['STONK_INDEX_SPLITS'])
+        print(f"Start Eval on group: {group}, index: {index}, method: {method}, symbol: {symbol} ... ")
 
+        eval_rewards, eval_profits, eval_running_profits, eval_total_profit = evaluate(model=model,
+                                                                                       index=index,
+                                                                                       symbol=symbol,
+                                                                                       dataset='valid',
+                                                                                       path=config['STONK_PATH'],
+                                                                                       splits=config['STONK_INDEX_SPLITS'])
+
+        group_total_profits.append(eval_total_profit)
         current_weights = f'weights/numq/{index}/{method}/{group}/numq_{symbol}_{i+1}.pt'
+
+        experiment_log[symbol] = {
+            'train': {
+                'losses': losses,
+                'rewards':rewards,
+                'val rewards': val_rewards,
+                'profits': profits,
+                'val profits': val_profits
+            },
+            'eval': {
+                'rewards': eval_rewards,
+                'profits': eval_profits,
+                'running profits': eval_running_profits,
+                'total profits': eval_total_profit
+            }
+        }
+        experiment_log["total profits"] += eval_total_profit
+
+        # plots_path = f'plots/numq/{index}/{method}/{group}/numq_{symbol}_{i+1}.pt'
+
         save_weights(model=model, OUT_PATH=current_weights)
 
-    return profits
+    print(f"Logging Experiment for group: {group}, index: {index}, method: {method} ... ")
 
-def evaluate_groups(groups: Dict, index:str, method: str, group:str):
-    # for i, symbol in enumerate(groups[index][method][group]):
-    # TODO: EVALUATE LAST MODEL IN EACH GROUP
-    #  SELECT BEST MODEL ACCORDING TO TOTAL VALID PROFIT
-    pass
+    log_experiment(experiment=experiment_log, filename=f"{index}.{method}.{group}")
+
+    return experiment_log
+def log_experiment(experiment:Dict, filename:str, path:str=config["EXPERIMENT_LOGS_PATH"]):
+    outpath = os.path.join(path, f"{filename}.json")
+    with open(outpath, 'w') as outfile:
+        json.dump(experiment, outfile)
+
+def evaluate_groups(groups: Dict):
+    best_group = ''
+    max_profit = -9999999.9
+    for index in groups:
+        for method in groups[index]:
+            for group in groups[index][method]:
+                print(f"Starting Experiment for group: {group}, index: {index}, method: {method} ... ")
+                log = pretrain_on_group(groups=groups, index=index, method=method, group=group)
+                name, profit = log["name"], log["total profits"]
+                if profit > max_profit:
+                    best_group, max_profit = name, profit
+
+    return best_group, max_profit
+
+def main():
+    groups = gather_groups()
+    best_group, max_profit = evaluate_groups(groups=groups)
 
 if __name__=='__main__':
     groups = gather_groups()
